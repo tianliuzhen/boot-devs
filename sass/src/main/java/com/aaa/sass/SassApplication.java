3ae23bd825587b9ff6c5724ffafc19bad31c37c9
tianliuzhen <tlzallen@163.com>
2021-09-28 00:27:58 +0800

    Springboot整合log4j2

--- sass/src/main/java/com/aaa/sass/SassApplication.java
+++ sass/src/main/java/com/aaa/sass/SassApplication.java
@@ -1,13 +1,16 @@
 package com.aaa.sass;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SassApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SassApplication.class, args);
+        log.info("SassApplication start success...");
     }
 
 }
