5fbc31db3dd1a57b64d924cb453503052af7f178
tianliuzhen <tlzallen@163.com>
2021-12-04 21:21:55 +0800

    自定义 springBot starter 测试 （完善）

--- tlz-spring-boot-starter/src/main/java/com/aaa/tlzspringbootstarter/common/Tlz2Properties.java
+++ tlz-spring-boot-starter/src/main/java/com/aaa/tlzspringbootstarter/common/Tlz2Properties.java
@@ -6,14 +6,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * 映射 yml或者properties 文件
+ * <p>
+ * 在 META-INF 下重写
+ * spring-configuration-metadata.json
+ * <p>
+ * 重写为了实现 hints：
+ * 是类似于Java中的枚举，只不过是给每一个属性的值配置一个说明，
+ * 方便使用者在配置的时候能够按照规定的值进行正确配置
  *
  * @author liuzhen.tian
  * @version 1.0 TlzProperties.java  2021/12/4 10:25
  */
 @Setter
 @Getter
-@ConfigurationProperties(prefix = "tlz")
-public class TlzProperties {
+@ConfigurationProperties(prefix = "tlz2")
+public class Tlz2Properties {
 
     /**
      * 是否启用
