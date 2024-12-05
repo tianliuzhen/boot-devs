5ca1fb3964506b63a22bf149688a4ae4aa863541
tianliuzhen <tlzallen@163.com>
2024-12-05 22:05:53 +0800

    quartz集成单独数据源

--- schedule/src/main/java/com/aaa/schedule/quartz/config/DataSourceConfig.java
+++ schedule/src/main/java/com/aaa/schedule/quartz/config/DataSourceConfig.java
@@ -16,7 +16,9 @@ import org.springframework.context.annotation.Primary;
 import javax.sql.DataSource;
 
 /**
+ * 参考：
  * https://www.cnblogs.com/bzq-nancy/p/13930880.html
+ * https://blog.csdn.net/goodjava2007/article/details/126982657
  *
  * @see org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration
  */
@@ -41,6 +43,12 @@ public class DataSourceConfig {
     /**
      * @QuartzDataSource 注解则是配置Quartz独立数据源的配置
      * 这里必须要加 @QuartzDataSource，否则，quartzDataSource.getIfAvailable();拿不到数据，JdbcStoreTypeConfiguration.quartzDataSourceInitializer 引用时保持对齐
+     * @QuartzDataSource的作用是什么？ Spring Boot （2.4.2）Reference Documentation
+     * Spring Boot （2.4.2）关于QUARTZ的相关说明
+     * <p>
+     * 当基于SpringBoot框架的项目与QUARTZ集成，且QUARTZ需要使用指定的数据源时，需要按下面的方案进行处理，其他的方案都不生效。@QuartzDataSource注解是SpringBoot提供的与QUARTZ集成时使用指定数据的方案。
+     * <p>
+     * To have Quartz use a DataSource other than the application’s main DataSource, declare a DataSource bean, annotating its @Bean method with @QuartzDataSource. Doing so ensures that the Quartz-specific DataSource is used by both the SchedulerFactoryBean and for schema initialization.
      */
     @Bean
     @QuartzDataSource
